3a9273ac4fc071d5d299738bb416c54965cc47ee
nobu <pyomta13@gmail.com>
2015-06-11 13:02:15 +0900

    タスク追加ボタンを追加

--- ToDoApp/ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/ToDoApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ToDoApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
