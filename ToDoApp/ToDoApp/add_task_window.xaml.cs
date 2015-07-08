8b02f8e045d7d0e0497206db3d4ea64ad01b81f8
NOBUAKI SATO <pyomta13@gmail.com>
2015-07-08 14:50:09 +0900

    キャンバス上にオブジェクトをDDできるようにした

--- ToDoApp/ToDoApp/add_task_window.xaml.cs
+++ ToDoApp/ToDoApp/add_task_window.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 
-
 namespace ToDoApp
 {
     /// <summary>
@@ -23,7 +22,7 @@ namespace ToDoApp
 
     public class TaskEventArgs:EventArgs
     {
-        public Task task = new Task(); 
+        public Task task = new Task();
     }
 
     public partial class add_task_window : Window
@@ -35,6 +34,8 @@ namespace ToDoApp
         public event EventHandler AddTask;
 
         public event TaskEventHandler AddTask2;
+
+        private bool is_drag = false;
 
 
         public add_task_window()
@@ -69,17 +70,35 @@ namespace ToDoApp
         private void main_canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             //MessageBox.Show(e.GetPosition(main_canvas).ToString());
-            
+
+            is_drag = true;
+            var x = e.GetPosition(main_canvas).X - miku.Width / 2;
+            var y = e.GetPosition(main_canvas).Y - miku.Height / 2;
+
+            Canvas.SetTop(miku, Math.Max(0,Math.Min(y, main_canvas.Height - miku.Height)));
+            Canvas.SetLeft(miku, Math.Max(0,Math.Min(x, main_canvas.Width - miku.Width)));
         }
 
         private void main_canvas_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            
+            is_drag = false;
         }
 
         private void main_canvas_PreviewMouseMove(object sender, MouseEventArgs e)
         {
+            if (is_drag||e.LeftButton == MouseButtonState.Pressed)
+            {
+                var x = e.GetPosition(main_canvas).X - miku.Width / 2;
+                var y = e.GetPosition(main_canvas).Y - miku.Height / 2;
 
+                Canvas.SetTop(miku, Math.Max(0, Math.Min(y, main_canvas.Height - miku.Height)));
+                Canvas.SetLeft(miku, Math.Max(0, Math.Min(x, main_canvas.Width - miku.Width)));
+            }
+        }
+
+        private void main_canvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            is_drag = false;
         }
     }
 }
